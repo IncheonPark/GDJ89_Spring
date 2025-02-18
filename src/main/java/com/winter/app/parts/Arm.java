b690f9bb8675f04d45bee2ee409664d2308da0cf
IncheonPark <dnehd8@gmail.com>
2025-02-18 14:49:26 +0900

    bean

---
+++ src/main/java/com/winter/app/parts/Arm.java
@@ -0,0 +1,9 @@
+package com.winter.app.parts;
+
+public interface Arm {
+	
+	
+	
+	public abstract void punch();
+
+}
---
+++ src/main/java/com/winter/app/parts/RightArm.java
@@ -0,0 +1,17 @@
+package com.winter.app.parts;
+
+import org.springframework.stereotype.Component;
+
+
+public class RightArm implements Arm {
+	
+	
+	
+	//
+	@Override
+	public void punch() {
+		System.out.println("펀치");
+		
+	}
+
+}
